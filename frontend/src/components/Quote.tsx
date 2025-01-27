4e0775f2246b5450664f9bfe644df4588915f43e
amriith <amrithjayadeep916@gmail.com>
2025-01-27 19:33:00 +1100

    Add routing and new pages for Signup, Signin, and Blog; introduce Quote component

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,12 @@
+export const Quote = () => {
+ return <div className="bg-gray-300 h-screen flex justify-center flex-col">
+            <div className="flex justify-center">
+          <div className=" max-w-md text-center bg-red-600">
+            Quote from Quote component
+            
+            </div> 
+          </div>
+        </div>
+        
+    
+}
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,8 @@
+import { Quote } from "../components/Quote"
+
+export const Signup = () => {
+return <div>
+             <Quote />
+            </div>
+      
+}
