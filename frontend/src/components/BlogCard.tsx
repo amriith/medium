45f40798f43e0e180351102b9adbf15dc2ca069e
amriith <amrithjayadeep916@gmail.com>
2025-02-08 12:55:16 +1100

    Enhance BlogCard component with improved layout and additional features; add Appbar component for navigation

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -13,38 +13,44 @@ export const BlogCard = ({
     publishedDate
 }: BlogCardProps) =>{
     return (
-        <div className="flex flex-col border border-slate-250">
-            
+        <div className="flex flex-col border rounded-2xl shadow-sm  pb-4 pt-4 border-slate-250">
+            <a href="#">
            <div className="flex items-center space-x-2"> 
-           <Avatar/> <div className="font-extralength pl-2 text-sm flex" > {authorName}  </div>  <div className="font-thin pl-2 text-slate-500" >{publishedDate} </div> 
+           <Avatar/> <div className="font-extralength pl-2 text-sm flex" > {authorName}  </div>    <div className="flex justify-center flex-col pl-2 flex justify-center flex-col">
+                    <Circle />
+                </div>  <div className="font-thin pl-2 text-slate-500" >{publishedDate} </div> 
             </div> 
             <div className="font-semibold text-xl">
                 {title}
             </div>
             <div className="text-slate-600 dark:text-slate-400">
-                {content.slice(0,70) + "...."}
+                {content.slice(0,120) + "...."}
             </div>
             <div>
-                {`${Math.ceil(content.length/100)} minutes read`}
+               <Badge title={title}/>   {`${Math.ceil(content.length/100)} minutes read`}
             </div>
+            </a>
         </div>
     )   
 }
-function Avatar() {
+function Badge({title}: Pick<BlogCardProps, "title">){
+    const secondWord = title.split(" ")[1] || title;
     return (
-      <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-        <svg
-          className="absolute w-12 h-12 text-gray-400 -left-1"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-            clipRule="evenodd"
-          ></path>
-        </svg>
-      </div>
+        <span className="bg-gray-100 text-black-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-gray-300 dark:text-black-300">{secondWord}</span>
+    )
+}
+export function Avatar() {
+    return (
+      <img
+        src="https://docs.material-tailwind.com/img/face-2.jpg"
+        alt="avatar"
+        className="relative inline-block h-[58px] w-[58px] !rounded-full object-cover object-center"
+      />
     );
   }
+
+  export function Circle() {
+    return <div className="h-1 w-1 rounded-full bg-slate-500">
+
+    </div>
+}
